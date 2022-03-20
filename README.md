52d8178943b597ef9acaf4760badb79ee5e38167
Phillip Cargo <phillip.r.cargo@gmail.com>
2022-03-20 12:58:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# WPF Hosted JS App
+
+## Description
+A Visual Studio 2022 Solution that creates an WPF based application that wraps a WebView2 component which loads a JavaScript application.
+
+## Prerequisite
+- Windows 10+
+- Visual Studio 2022
+- NodeJS 16
+
+## References
+- [Windows Presentation Foundation](https://en.wikipedia.org/wiki/Windows_Presentation_Foundation)
+- [WebView2](https://docs.microsoft.com/en-us/microsoft-edge/webview2/)
+- [React](https://reactjs.org/)
+
+## Challenges
+### Building the JavaScript application in Visual Studio
+A JavaScript project name with capital letters in the name caused build errors
+
+### Setup the JavaScript application to build
+By default there is not build command defined
+
+- Open project properties
+- Build > General > Build Command
+  - `npm run build`
+
+### Including the JavaScript application files in the WpfHost
+Visual Studio does not have a UI for linking a folder.
+
+- Unload the WpfHost project
+- Edit Project File
+
+```xml
+<ItemGroup>
+    <Content Include="..\jsapp\build\**\*.*">
+        <Link>jsapp\%(RecursiveDir)%(FileName)%(Extension)</Link>
+        <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </Content>
+</ItemGroup>
+```
+
---
+++ WpfHost/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Web.WebView2.Core;
+using System;
+using Microsoft.Web.WebView2.Wpf;
+
+namespace WpfHost
+{
+    /// <summary>
+    /// Provide the display for the CardList
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // subscribe to unload event
+            this.Unloaded += CardListDisplayControl_Unloaded;
+            InitializeAsync();
+        }
+
+        /// <summary>
+        /// Handle disposing of WebView2
+        /// </summary>
+        private void CardListDisplayControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            webView.Dispose();
+        }
+
+        /// <summary>
+        /// Setup WebView2 - waiting for it to initalize
+        /// </summary>
+        async void InitializeAsync()
+        {
+
+            // wait for the view to initialize
+            await webView.EnsureCoreWebView2Async(null);
+
+            // handle when navigation is complete
+            webView.NavigationCompleted += WebView_NavigationCompleted;
+
+            // map a folder to allow WebView2 to navigate to the folder
+            webView.CoreWebView2.SetVirtualHostNameToFolderMapping(
+                "app.example",
+                "jsapp",
+                CoreWebView2HostResourceAccessKind.Deny
+                );
+
+            // load file web app from mapped folder
+            webView.Source = new Uri("http://app.example/index.html");
+
+        }
+
+        /// <summary>
+        /// Indicate if the first navigation is complete
+        /// </summary>
+        private Boolean webViewFirstNavigationCompleted = false;
+
+        /// <summary>
+        /// Handle when WebView2 completes navigating
+        /// </summary>
+        private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            // upon successful navigation make the webView visible
+            if (!webViewFirstNavigationCompleted && e.IsSuccess)
+            {
+                webViewFirstNavigationCompleted = true;
+                webView.Visibility = Visibility.Visible;
+            }
+            webView.Visibility = Visibility.Visible;
+        }
+
+    }
+}
+
